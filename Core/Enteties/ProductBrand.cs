e650cec69b0b16ef9ce7d29c348e7d8f804beb33
letsoalos <s3llo1@icloud.com>
2023-02-27 16:17:48 +0200

    End of section 3

---
+++ Core/Enteties/ProductBrand.cs
@@ -0,0 +1,9 @@
+using Core.Interfaces;
+
+namespace Core.Enteties
+{
+    public class ProductBrand : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Enteties/ProductType.cs
@@ -0,0 +1,9 @@
+using Core.Interfaces;
+
+namespace Core.Enteties
+{
+    public class ProductType : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
